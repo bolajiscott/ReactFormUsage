53f74ef5c26d4fdf10612cbb1d8df24bbc9f4f2c
bolajiscott <bolajiscott@yahoo.com>
2019-10-22 15:08:32 +0100

    componets-added

---
+++ UserForm.jsx
@@ -0,0 +1,37 @@
+class LoginForm extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state = { username: '', password: '' };
+    }
+   
+    handleChange = ({ target }) => {
+       this.setState({ [target.name]: target.value });
+    };
+   
+    render() {
+      return (
+        <React.Fragment>
+          <form>
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+   
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </form>
+   
+          <h3>Your username is: {this.state.username}</h3>
+        </React.Fragment>
+      );
+    }
+   }
